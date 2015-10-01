d345516b1088aa0a3ecf1b9f8f3f68a8c0566a0f
Nacho Álvarez <ignacioalvarez92@gmail.com>
2015-10-01 08:14:45 -0300

    Minor fixes.

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const network = require('network');
 const app = express();
 
 const PORT = 25792;
+var IP;
 
 app.listen(PORT, function () {
   console.log(`Server running at http://localhost:${PORT}/`);
+});
+
+network.get_public_ip(function(err, ip) {
+  console.log(ip);
+  IP = err || ip;
 });
 
 let drop = document.getElementsByTagName('body')[0];
@@ -21,6 +28,7 @@ function handleDrop(e) {
     res.download(file.path);
   });
 
+  document.getElementById('title').innerHTML = `Ready! Go to <span>${IP}:${PORT}</span> to download!`;
   // Clear dragging style
   drop.classList.remove('dragover');
 }
